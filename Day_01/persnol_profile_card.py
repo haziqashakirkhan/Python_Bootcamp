b61d44409ec6746c6cd7fd4ec2d116b31e3f4fc9
Haziqa Shakir khan <haziqashakir@gmail.com>
2026-07-24 23:26:43 +0500

    Day_01:variables,input,if/else/elif

---
+++ Day_01/persnol_profile_card.py
@@ -0,0 +1,16 @@
+name = input("What is your name? ")
+age = input("What is your age? ")
+language = input("What is your favorite programming language? ")
+city = input("What city do you live in? ")
+job = input("What is your Dream job? ")
+
+print("===============================")
+print("PERSONAL PROFILE CARD")
+print("===============================")
+
+print("ASSALAMUALIKUM!", name)
+
+print( "age is", age)  
+print(" favorite programming language is", language) 
+print(" lives in", city)
+print("dream job", job)
